73ffdf161ab836b43605b0924df6669bb027dfdf
https://github.com/hesaaam/hesaaam.git <https://github.com/hesaaam/hesaaam.git>
2023-07-11 01:09:23 +0330

    update movie card ui

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -10,32 +10,43 @@ export const createMovieCard = (movie) => {
     title,
     vote_average,
     release_date,
-    id
+    id,
+    overview
   } = movie;
 
   const card = document.createElement('div');
   card.classList.add('movie_card');
 
-  card.innerHTML = `
-      <figure class="poster_box card_banner">
-        <img src="${imageBaseURL}w342${poster_path}" alt="${title} " class="img_cover" loading="lazy"/>
-      </figure>
-      <h4 class="title">${title}</h4>
+    card.innerHTML = `
+
       
-      <div class="meta_list">
-        <div class="meta_item">
-          <img src="/dist/img/star.png" width="20" height="20" loading="lazy" alt="rating" />
-          <span class="span">${vote_average.toFixed(1)}</span>
-        </div>
-      
-        <div class="card_badge">${release_date.split('-')[0]}</div>
+     <div class="card_head">
+       <figure class="poster_box card_banner">
+       <img src="${imageBaseURL}w342${poster_path}" alt="${title} " class="img_cover" loading="lazy"/>
+       </figure>
+       <a href="/detail" @click.prevent="changeRoute('/detail'); getMovieDetails(${id})" 
+        class="card_btn" title="${title}">
+          <div class="meta_list">
+            <div class="meta_item">
+              <img src="/dist/img/star.png" width="20" height="20" loading="lazy" alt="rating" />
+              <span class="span">${vote_average.toFixed(1)}</span>
+            </div>
+
+            <div class="card_badge">${release_date.split('-')[0]}</div>
+
+          </div>
+          <p class="movie_text">${overview}</p>
+          <div class="play">
+          <img src="/dist/img/play.png" alt="ply"/>
+          </div>
+
+          <!-- getMovieDetails() called from store/store.js -->
+        </a>
+
+
       </div>
-      
-      <a href="/detail" @click.prevent="changeRoute('/detail'); getMovieDetails(${id})" 
-       class="card_btn" title="${title}"> <!-- getMovieDetails() called from store/store.js -->
-      </a>
-  
-  `
+      <h4 class="title">${title}</h4>
+    `
 
   return card;
 }
--- src/pages/movieListPage.js
+++ src/pages/movieListPage.js
@@ -21,24 +21,29 @@ const movieListPage = () => {
               <template x-if="movieList" >
                 <template x-for="movie in movieList" :key="movie.id">
                   <div class="movie_card">
+                    <div class="card_head">
                       <figure class="poster_box card_banner">
                         <img :src="imageBaseURL+'w342'+movie.poster_path" :alt="movie.title " class="img_cover" loading="lazy"/>
                       </figure>
-                      <h4 class="title" x-jsz>{{movie.title}}</h4>
-          
-                      <div class="meta_list">
-                        <div class="meta_item">
-                          <img src="/dist/img/star.png" width="20" height="20" loading="lazy" alt="rating" />
-                          <span class="span" x-jsz>{{movie.vote_average.toFixed(1)}}</span>
-                        </div>
                       
-                        <div class="card_badge" x-jsz>{{movie.release_date.split('-')[0]}}</div>
-                      </div>
-          
                       <a href="/detail" @click.prevent="changeRoute('/detail'); getMovieDetails(movie.id)" 
-                      class="card_btn" :title="movie.title"> <!-- getMovieDetails() called from store/store.js -->
+                      class="card_btn" :title="movie.title">
+                        <div class="meta_list">
+                          <div class="meta_item">
+                            <img src="/dist/img/star.png" width="20" height="20" loading="lazy" alt="rating" />
+                            <span class="span" x-jsz>{{movie.vote_average.toFixed(1)}}</span>
+                          </div>
+                        
+                          <div class="card_badge" x-jsz>{{movie.release_date.split('-')[0]}}</div>
+                        </div>
+                        <p class="movie_text" x-jsz>{{movie.overview}}</p>
+                        <div class="play">
+                          <img src="/dist/img/play.png" alt="ply"/>
+                        </div>
+                        <!-- getMovieDetails() called from store/store.js -->
                       </a>
-                  
+                    </div>
+                    <h4 class="title" x-jsz>{{movie.title}}</h4>
                   </div>
                 </template>
               
@@ -51,7 +56,7 @@ const movieListPage = () => {
               
             </div>
 
-            <button x-bind="loadMore" class="btn load_more">بارگذاری بیشتر</button>
+            <button x-bind="loadMore" class="btn load_more">بیشتر</button>
           </section>
 
 
